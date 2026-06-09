050f8e3352cb41e0bb8026ab78c018567766494e
Jason Heddings <jason.heddings@workday.com>
2026-06-09 14:52:32 -0600

    feat(data): add RestClient primitive over HttpClient

---
+++ src/data/RestClient.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from './HttpClient.js';
+
+export interface RestRequestOptions {
+  headers?: Record<string, string>;
+  signal?: AbortSignal;
+}
+
+export class RestClient {
+  private readonly http: HttpClient;
+
+  constructor(client: HttpClient | string = '') {
+    this.http = typeof client === 'string' ? new HttpClient(client) : client;
+  }
+
+  get<T>(path: string, opts: RestRequestOptions = {}): Promise<T> {
+    return this.http.request<T>(path, { method: 'GET', headers: opts.headers, signal: opts.signal });
+  }
+
+  post<T>(path: string, body?: unknown, opts: RestRequestOptions = {}): Promise<T> {
+    return this.http.request<T>(path, { method: 'POST', body, headers: opts.headers, signal: opts.signal });
+  }
+
+  put<T>(path: string, body?: unknown, opts: RestRequestOptions = {}): Promise<T> {
+    return this.http.request<T>(path, { method: 'PUT', body, headers: opts.headers, signal: opts.signal });
+  }
+
+  patch<T>(path: string, body?: unknown, opts: RestRequestOptions = {}): Promise<T> {
+    return this.http.request<T>(path, { method: 'PATCH', body, headers: opts.headers, signal: opts.signal });
+  }
+
+  delete<T>(path: string, opts: RestRequestOptions = {}): Promise<T> {
+    return this.http.request<T>(path, { method: 'DELETE', headers: opts.headers, signal: opts.signal });
+  }
+}
